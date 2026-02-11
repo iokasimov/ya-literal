d617b98dafcb236dbad0f5c2d960af68baa68e96
Murat Kasimov <iokasimov.m@gmail.com>
2026-02-11 21:49:01 +0700

    Define `IsList` instance for `Maybe`

---
+++ Ya/Literal/Maybe.hs
@@ -0,0 +1,10 @@
+module Ya.Literal.Maybe where
+
+import Ya
+
+import "base" GHC.IsList (IsList (Item, toList, fromList))
+
+instance IsList (Maybe i) where
+ type Item (Maybe i) = i
+ fromList [] = Empty `hc` Unit
+ fromList [x] = Exist `hc` x
