20db284aa489c9a50a9da44569f5452d58191579
Murat Kasimov <iokasimov.m@gmail.com>
2026-04-29 19:41:23 +0700

    Reflect changes from core library

--- Ya/Literal/List.hs
+++ Ya/Literal/List.hs
@@ -27,7 +27,7 @@ instance IsList (List i) where
   worker (c : []) = Item c `ha` Last `har` Unit
   worker (c : cs) = Item c `ha` Next `har` worker cs
  toList xs = that @[_] `har___` xs
-  `yokl` Prior `ha` Apply `ha` State `ha` Event `ha_` (:) `ho'ho` fetch
+  `yokl` Prior `ha` Apply `ha` State `ha` Event `ha_` (:) `ho'ho` Pull
   `bt'har__` []
 
 instance IsList (Twice `T'TT'I` List `T'I_` i) where
